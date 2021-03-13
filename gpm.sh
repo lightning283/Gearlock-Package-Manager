0b272ac070b876a7e694ca68855e18b2583519f5
Xtr126 <80520774+Xtr126@users.noreply.github.com>
2021-03-13 15:03:56 +0530

    Update gpm.sh

--- gpm.sh
+++ gpm.sh
@@ -2,7 +2,7 @@
 
 URL="https://raw.githubusercontent.com/LIGHTNING283/Gearlock-Package-Manager/main/Links.txt?token=ATGKMRXOTNUIMEKDWX56NZTAJSCIU"
         case $1 in 
-            list) echo "Updating list of packages"; echo $(axel $URL && cat Links.txt)
+            list) echo "Updating list of packages" && echo $(axel $URL && cat Links.txt)
             ;;
             
             *) echo "Invalid option!"                
