11e3d0b37f77698bf567e2955ecd090b705d18c2
Xtr126 <80520774+Xtr126@users.noreply.github.com>
2021-03-13 15:05:49 +0530

    Update gpm.sh

--- gpm.sh
+++ gpm.sh
@@ -1,6 +1,6 @@
 #!/gearlock/bin/bash
 
-URL="https://raw.githubusercontent.com/LIGHTNING283/Gearlock-Package-Manager/main/Links.txt?token=ATGKMRXOTNUIMEKDWX56NZTAJSCIU"
+URL="https://raw.githubusercontent.com/"
         case $1 in 
             list) echo "Updating list of packages" && echo $(axel $URL && cat Links.txt)
             ;;
